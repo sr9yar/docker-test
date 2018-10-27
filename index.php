270cc9a51833ba094a40cf8acc03c018f5eb55e7
sr9yar <sr9yar@users.noreply.github.com>
2018-10-27 20:26:08 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?php echo "hello";
+<?php echo "hello<br>";
+
+try{
+  $dbh = new pdo( 'mysql:host=127.0.0.1:3306;dbname=root',
+                  'root',
+                  'root',
+                  array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+  echo "true<br>";
+  //die(json_encode(array('outcome' => true)));
+}
+catch(PDOException $ex){
+  echo "false<br>";
+  //die(json_encode(array('outcome' => false, 'message' => 'Unable to connect')));
+}
