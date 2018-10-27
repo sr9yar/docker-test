4fa211c63f69eb3c2004459c5eaf344e9d18127f
sr9yar <sr9yar@users.noreply.github.com>
2018-10-27 21:14:06 +0300

    Update index.php
    pdo conn test

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php echo "hello<br>";
 
 try{
-  $dbh = new pdo( 'mysql:host=127.0.0.1:3306;dbname=root',
+  $dbh = new pdo( 'mysql:host=mysql:3306;dbname=root',
                   'root',
                   'root',
                   array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-  echo "true<br>";
+  echo "mysql connection test: true<br>";
   //die(json_encode(array('outcome' => true)));
 }
 catch(PDOException $ex){
-  echo "false<br>";
+  echo "mysql connection test: false<br>";
   //die(json_encode(array('outcome' => false, 'message' => 'Unable to connect')));
 }
