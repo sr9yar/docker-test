3ca63d086e1870909191d094223fefffe64415ed
sr9yar <sr9yar@users.noreply.github.com>
2018-10-27 23:57:57 +0300

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,9 @@ try{
   //die(json_encode(array('outcome' => true)));
 }
 catch(PDOException $ex){
-  echo "mysql connection test: false<br>";
+  echo "mysql connection test: false<br><br><br><br><br>";
   //die(json_encode(array('outcome' => false, 'message' => 'Unable to connect')));
 }
+
+phpinfo(); 
+
